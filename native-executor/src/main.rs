23ae89c74e2cf986a132a7271f707dc7e73c398a
Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
2024-11-07 05:15:13 -0300

    Update docs

--- native-executor/src/main.rs
+++ native-executor/src/main.rs
@@ -1,31 +1,48 @@
 use std::mem::MaybeUninit;
 
-use wasmtime::{Config, Engine, Instance, Module, OptLevel, Memory, MemoryType, Caller, Func, Store, AsContext};
+use wasmtime::{
+    AsContext, Caller, Config, Engine, Func, Instance, Memory, MemoryType, Module, OptLevel, Store,
+};
 
 // Código WebAssembly em formato de texto.
 const WAT_CODE: &[u8] = include_bytes!("../../wasm_runtime.wat");
 
-struct State {
-    // `MaybeUninit` é utilizado para inicializar o estado com um valor nulo.
-    // Ex: A depende de B, e B depende de A, então é necessário inicializar um dos dois com um valor nulo.
-    memory: MaybeUninit<Memory>,
+
+pub struct State {
+    pub memory: Memory,
 }
 
 impl State {
-    #[must_use]
-    pub const fn new() -> Self {
-        Self {
-            memory: MaybeUninit::uninit(),
+    #[allow(invalid_value, clippy::missing_errors_doc)]
+    /// Inicializa o estado, store e memória.
+    pub fn new(engine: &Engine, memory_type: MemoryType) -> anyhow::Result<Store<Self>> {
+        // O `Store` precisa do `State` para ser criado, e a memória precisa do `Store` para ser
+        // criada, e o `State` precisa da memória, logo temos um dependencia cíclica, para
+        // resolver esse dilema o rust nos fornece o tipo `MaybeUninit`, que é utilizado
+        // para inicializar um valor com um valor "nulo" que é parecido com um `null`, porém
+        // dever ser utilizado com cuidado, depois de criado pode inicializar o `State`.
+        // Referencias:
+        // - https://github.com/bytecodealliance/wasmtime/issues/4922#issuecomment-1251086171
+        // - https://doc.rust-lang.org/std/mem/union.MaybeUninit.html
+        #[allow(clippy::uninit_assumed_init)]
+        let state = Self {
+            // SAFETY: A memória será inicializada corretamente abaixo.
+            memory: unsafe { MaybeUninit::<Memory>::zeroed().assume_init() },
+        };
+
+        // Agora podemos criar o `Store` com o `State`.
+        let mut store = Store::new(engine, state);
+
+        // Agora podemos criar a memória e inicializar o `State`.
+        // Ref: https://doc.rust-lang.org/std/mem/union.MaybeUninit.html#initializing-a-struct-field-by-field
+        unsafe {
+            // let mut jose = MaybeUninit::<State>::uninit();
+            // jose.write(val)
+            // let store = std::mem::transmute::<Store<MaybeUninit<State>>, Store<State>>(store);
+            std::ptr::addr_of_mut!(store.data_mut().memory).write(Memory::new(&mut store, memory_type)?);
         }
-    }
 
-    pub fn init(&mut self, memory: Memory) {
-        self.memory = MaybeUninit::new(memory);
-    }
-
-    pub const fn memory(&self) -> &Memory {
-        // SAFETY: `memory` foi inicializado.
-        unsafe { self.memory.assume_init_ref() }
+        Ok(store)
     }
 }
 
@@ -56,10 +73,6 @@ fn main() -> anyhow::Result<()> {
     //////////////////////////////////
     let module = Module::new(&engine, WAT_CODE)?;
 
-    // Inicia um Store, utilizado para compartilhar um estado entre
-    // o host e o WebAssembly. (não é necessário para este exemplo)
-    let mut store = Store::new(&engine, State::new());
-
     // Configura a memória que será utilizada pelo WebAssembly.
     //
     // Cada página tem 64KB, aqui foi configurado 2 página de memória, que pode ser
@@ -67,10 +80,10 @@ fn main() -> anyhow::Result<()> {
     // chamando de dentro do webassembly o método `core::arch::wasm32::memory.grow`.
     // ref: https://doc.rust-lang.org/core/arch/wasm32/fn.memory_grow.html
     let memory_type = MemoryType::new(2, Some(16));
-    let memory = Memory::new(&mut store, memory_type)?;
 
-    // Inicializa o estado com a memória criada.
-    store.data_mut().init(memory);
+    // Inicia um Store, utilizado para compartilhar um estado entre
+    // o host e o WebAssembly.
+    let mut store = State::new(&engine, memory_type)?;
 
     // Define uma função que pode ser chamada pelo WebAssembly.
     #[allow(clippy::cast_possible_truncation)]
@@ -78,15 +91,12 @@ fn main() -> anyhow::Result<()> {
         // Convert o `Caller` para um contexto, que utilizaremos para ler a memória.
         let ctx = caller.as_context();
 
-        // Le a memória do host.
-        let memory = ctx.data().memory();
-
         // Define o intervalo de memória que será lido.
         let start = usize::try_from(offset).unwrap_or(usize::MAX);
         let end = start.saturating_add(len as usize);
 
         // Verifica se o intervalo de memória está dentro dos limites da memória.
-        let Some(bytes) = memory.data(&ctx).get(start..end) else {
+        let Some(bytes) = ctx.data().memory.data(&ctx).get(start..end) else {
             anyhow::bail!("out of bounds memory access");
         };
 
@@ -102,7 +112,8 @@ fn main() -> anyhow::Result<()> {
         Ok(())
     });
 
-    // Imports do módulo WebAssembly
+    // Imports do módulo WebAssembly.
+    let memory = store.data().memory;
     let imports = [memory.into(), hello_func.into()];
 
     // Cria uma instância do módulo WebAssembly
